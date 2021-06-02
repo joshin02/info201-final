c61b8e9b6320b4697eba11231d3eec2997b7f3a4
Vu Le <vule010@uw.edu>
2021-06-02 15:43:24 -0700

    added line graph in main panel and widgets that changes the graph

--- NBAAnalysis/server.R
+++ NBAAnalysis/server.R
@@ -3,7 +3,19 @@ library(tidyverse)
 
 data <- read.csv("../nba-data/2000-20.csv")
 
+data <- data %>%
+  group_by(year) %>%
+  mutate(avgAttempt = sum(X3PA) / nrow(data)) %>%
+  mutate(avgMade = sum(X3P) / nrow(data)) %>%
+  mutate(avgPercent = sum(X3P.) / nrow(data))
+
+
 shinyServer(function(input, output) {
     output$bargraph <- renderPlot({
+      
+      ggplot(data) + 
+        geom_line(aes_string(x = "year", y = input$yVar)) +
+        labs(x = "Year", y = "Average", title = "Average 3 point attempts/made/percentage in the NBA")
+      
     })
 })
--- NBAAnalysis/ui.R
+++ NBAAnalysis/ui.R
@@ -6,13 +6,27 @@ shinyUI(fluidPage(
     titlePanel("3 Point Attempts/Percentage Through the Years"),
 
     # widgets
-    sidebarLayout(
-        sidebarPanel(
-            
+    tabsetPanel(
+        tabPanel("1", fluid = T,
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput("yVar", label = "Type of Data",
+                                     choices = list("Average 3 point made" = "avgMade",
+                                                    "Average 3 point attempts" = "avgAttempt",
+                                                    "Average 3 point percentage" = "avgPercent"),
+                                     selected = "Average 3 point attempts"),
+                         
+                     ),
+                     # show plot
+                     mainPanel(
+                         plotOutput("bargraph")
+                     )  
+                 )
         ),
-        # show plot
-        mainPanel(
-            plotOutput("bargraph")
-        )
-    )
+        
+        tabPanel("2", fluid = T)
+        
+    ),
+    
+   
 ))
